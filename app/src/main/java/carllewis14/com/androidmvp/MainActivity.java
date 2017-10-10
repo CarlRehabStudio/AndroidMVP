3aaa0f8453e70cf37af06f7a5b732c5f27ada466
Carl Lewis <carl.lewis@rehabstudio.com>
2017-10-10 17:00:11 +0100

    dependecy update

--- app/src/main/java/carllewis14/com/androidmvp/MainActivity.java
+++ app/src/main/java/carllewis14/com/androidmvp/MainActivity.java
@@ -5,6 +5,8 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    private String TestName = "Carl";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
